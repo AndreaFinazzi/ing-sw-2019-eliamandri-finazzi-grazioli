10f586bb35fbbebc4b490293ab276c92bd8603b9
AndreaFinazzi <af.andreafinazzi@gmail.com>
2019-03-19 21:51:50 +0100

    Project set-up

---
+++ adrenaline/src/main/java/it/polimi/se/eliafinazzigrazioli/adrenaline/Adrenaline.java
@@ -0,0 +1,14 @@
+package it.polimi.se.eliafinazzigrazioli.adrenaline;
+
+import java.util.Date;
+
+public class Adrenaline {
+
+    public Adrenaline() {
+        this.playTheGame();
+    }
+
+    private Date playTheGame() {
+        return new Date();
+    }
+}
