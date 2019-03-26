2932c4af4d333703c97d95bc195cd6389f02021f
AndreaFinazzi <af.andreafinazzi@gmail.com>
2019-03-26 17:04:52 +0100

    module 'Adrenaline' removed, packages moved to root folder.

--- src/main/java/it/polimi/se/eliafinazzigrazioli/adrenaline/Adrenaline.java
+++ src/main/java/it/polimi/se/eliafinazzigrazioli/adrenaline/Adrenaline.java
@@ -3,7 +3,7 @@ package it.polimi.se.eliafinazzigrazioli.adrenaline;
 import java.util.Date;
 
 public class Adrenaline {
-
+    // Base commit, test commit
     public Adrenaline() {
         this.playTheGame();
     }
