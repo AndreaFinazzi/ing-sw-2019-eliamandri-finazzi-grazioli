8afa55bc73edf50fb46e1f39aa89a95e841e8e48
AndreaFinazzi <af.andreafinazzi@gmail.com>
2019-04-18 11:43:21 +0200

    Exception package. Events-handling exceptions added.

---
+++ src/main/java/it/polimi/se/eliafinazzigrazioli/adrenaline/exceptions/events/HandlerNotImplementedException.java
@@ -0,0 +1,8 @@
+package it.polimi.se.eliafinazzigrazioli.adrenaline.exceptions.events;
+
+public class HandlerNotImplementedException extends Exception {
+
+    public HandlerNotImplementedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/it/polimi/se/eliafinazzigrazioli/adrenaline/exceptions/events/ListenerNotFoundException.java
@@ -0,0 +1,7 @@
+package it.polimi.se.eliafinazzigrazioli.adrenaline.exceptions.events;
+
+public class ListenerNotFoundException extends Exception {
+    public ListenerNotFoundException(String message) {
+        super(message);
+    }
+}
