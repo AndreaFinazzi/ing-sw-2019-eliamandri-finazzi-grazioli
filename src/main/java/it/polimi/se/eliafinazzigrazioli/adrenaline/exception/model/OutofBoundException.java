17ca4b24662eaa5467420080a3b26bd4b280fb47
antoninoelia <antonino.elia@mail.polimi.it>
2019-04-18 22:39:42 +0200

    Exception package: AmmoNotAvaibleException, OutofBandException added

---
+++ src/main/java/it/polimi/se/eliafinazzigrazioli/adrenaline/exception/model/OutofBoundException.java
@@ -0,0 +1,11 @@
+package it.polimi.se.eliafinazzigrazioli.adrenaline.exception.model;
+
+public class OutofBoundException extends Exception {
+    public OutofBoundException() {
+        super();
+    }
+
+    public OutofBoundException(String message) {
+        super (message);
+    }
+}
